eaa26ca14919d048f3355120224f4a69e6477de3
TruongManh <manhcntt2.1@gmail.com>
2019-12-25 08:42:34 +0700

    tien xu ly tieng viet

---
+++ tien_xu_ly/test.py
@@ -0,0 +1,21 @@
+
+# coding=utf-8
+
+# import json
+
+# data = []
+
+# with open('data_.jsonl', 'r') as json_data:
+#     for f in json_data:
+#     	# tmp = f['raw'].split()
+#     	data.append(json.loads(f))    
+# # i = 0
+
+# for d in data:
+# 	d['raw'] = d['raw'].split()
+# 	d['original'] = d['original'].split()
+# 	# print(d)
+# print(data[1])
+
+a = ['ấ']
+print(a)
